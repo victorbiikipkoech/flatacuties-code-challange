871c0361e5f107a974d2c9730ff8227469619517
VICTOR <victor.kipkoech@student.moringaschool.com>
2023-10-20 23:09:30 +0300

    completed

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# FLATACUTIES CODE CHALLAGE
+# FLATACUTIES CODE CHALLANGE
+voting for the cutest animal .
+
+Getting Started
+create a db.json file and use this dataLinks to an external site. for your server DB.
+Run the following command to get the backend started:
+          json-server --watch db.json
+
+Test your server by visiting this route in the browser:
+          http://localhost:3000/characters
+License
+
+The Flatiron School, Inc. owns this Educational Content. However, the Flatiron
+School supports the development and availability of educational materials in
+the public domain. Therefore, the Flatiron School grants Users of the Flatiron
+Educational Content set forth in this repository certain rights to reuse, build
+upon and share such Educational Content subject to the terms of the Educational
+Content License set forth [here](http://learn.co/content-license)
+(http://learn.co/content-license). You must read carefully the terms and
+conditions contained in the Educational Content License as such terms govern
+access to and use of the Educational Content.
+
+Flatiron School is willing to allow you access to and use of the Educational
+Content only on the condition that you accept all of the terms and conditions
+contained in the Educational Content License set forth
+[here](http://learn.co/content-license) (http://learn.co/content-license).  By
+accessing and/or using the Educational Content, you are agreeing to all of the
+terms and conditions contained in the Educational Content License.  If you do
+not agree to any or all of the terms of the Educational Content License, you
+are prohibited from accessing, reviewing or using in any way the Educational
+Content.
+
+
+
--- index.js
+++ index.js
@@ -1,30 +1,82 @@
- function getCharacters(){
-  fetch("http://localhost:3000/characters", {
-      method:"GET",
-      headers:{
-        "content-Type":application/json"
-      }function
-  }).then(data => db.json())
-  .then( response => {
-      Characters = [...response]
-      displayCharacters(response)
-  })
-  function displayCharacters(characters) {
-      const characterbar = document.querySelector("#character-bar")
-      for( character of response){
-          console.log(character.name)
-          const.span = document.createElement("span");
-          span.innerText = character.name;
-          span.setAttribute("id",character.id)
-  
-          span.addEventListener("click" (Event))=>{
-              alert(Event.target.innerText)
-          })
-          characterbar.appendChild(span);
+// Define a variable to keep track of the total votes
+let totalVotes = 0;
+
+function fetchAndDisplayCharacters() {
+  const url = 'http://localhost:3000/characters';
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-  } 
-function getCharactersById(characters,id){
-    return characters.find((character)=>{
-        return character.id === id
+      return response.json();
     })
+    .then(data => {
+      // Display the names, images, and vote buttons on the HTML page
+      displayCharacterData(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
+
+function displayCharacterData(characters) {
+  const characterList = document.getElementById('character-list');
+  const characterImage = document.getElementById('character-image-element');
+  const totalVotesButton = document.getElementById('total-votes-button');
+  const totalVotesCount = document.getElementById('total-votes-count');
+
+  characterList.innerHTML = ''; // Clear previous content
+
+  characters.forEach(character => {
+    const listItem = document.createElement('li');
+    
+    // Create a element for the character name
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = character.name;
+
+    // Attach a click event to the name to display the character image
+    nameSpan.addEventListener('click', () => {
+      displayCharacterImage(character.image);
+    });
+
+    // Create a vote button
+    const voteButton = document.createElement('button');
+    voteButton.textContent = 'Vote';
+    voteButton.addEventListener('click', () => {
+      // Increment the totalVotes variable and update the display
+      totalVotes++;
+      totalVotesCount.textContent = 'Total Votes: ' + totalVotes;
+    });
+
+    listItem.appendChild(nameSpan);
+    listItem.appendChild(voteButton);
+    characterList.appendChild(listItem);
+  });
+
+  // Event listener for the "Total Votes" button
+  totalVotesButton.addEventListener('click', () => {
+    totalVotesCount.textContent = 'Total Votes: ' + totalVotes;
+  });
+}
+
+function displayCharacterImage(imageUrl) {
+  const characterImage = document.getElementById('character-image-element');
+  characterImage.src = imageUrl;
+}
+
+
+
+function resetVotes() {
+  totalVotes = 0;
+  const totalVotesCount = document.getElementById('total-votes-count');
+  totalVotesCount.textContent = 'Total Votes: ' + totalVotes;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const resetVotesButton = document.getElementById('reset-votes-button');
+  resetVotesButton.addEventListener('click', resetVotes);
+
+  // Call the function to fetch and display character data
+  fetchAndDisplayCharacters();
+});
